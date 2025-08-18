55e12ee7aae4d9d5d3898259685f11b34289ea82
Your NameChristian <christianhermanndev@gmail.com>
2025-08-18 16:13:12 -0500

    Fizzbuzz, resverseString, twoSums

--- script.js
+++ script.js
@@ -1,1 +1,52 @@
-console.log("This is my first LeetCode test!");
+console.log("Console test!");
+
+for (let i = 0; i <= 20; i++) {
+  if (i % 5 === 0 && i % 3 === 0) {
+    console.log("Fizzbuzz");
+  } else if (i % 3 === 0) {
+    console.log("Fizz");
+  } else if (i % 5 === 0) {
+    console.log("Buzz");
+  } else {
+    console.log(i);
+  }
+}
+
+/*
+I: string
+O: string reversed
+C: NA
+E: NA
+*/
+
+function reverseString(str) {
+  let chars = str.split("");
+  let reversed = chars.reverse();
+  return reversed.join("");
+}
+console.log(reverseString("hello"));
+
+// Two Sum //
+// given an array of integers nums and an integer target,
+// return the indices of the two numbers that add up to target.
+// Assume exactly one solution, and you can’t use the same element twice.
+// ([2, 7, 11, 15], 9))
+
+function twoSums(nums, target) {
+  // create an empty hash map to remember numbers that we've seen
+  // seen will store numbers we've seen as KEYS, and their indices as VALUES
+  const seen = {};
+
+  for (let i = 0; i < nums.legth; i++) {
+    const value = nums[i]; // current number
+    const need = target - value; // what number would complete the sum?
+
+    // If we've seen the "need" before, return the pair of indices
+    if (seen[need] !== undefined) {
+      return [seen[need], i];
+    }
+    // Otherwise, record this number and its index
+    seen[value] = i;
+  }
+  return []; // if no pair is found return empty array
+}
